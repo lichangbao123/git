1909330d32f99e2025adba817c147d260748628a
17597 <1759731988@qq.com>
2022-01-14 21:29:51 +0800

    2次提交

---
+++ test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @Auther: 李长宝
+ * @Date: 2022/1/14
+ * @Description: com.itheima.git
+ * @version: 1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    public void test(){
+        System.out.println("test");
+    }
+
+}
