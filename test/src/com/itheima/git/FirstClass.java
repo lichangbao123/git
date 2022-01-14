f191dc6a58e1e227f7d0490d774752b4e535552b
lichangbao123 <75620168+lichangbao123@users.noreply.github.com>
2022-01-14 22:42:06 +0800

    Update FirstClass.java

--- test/src/com/itheima/git/FirstClass.java
+++ test/src/com/itheima/git/FirstClass.java
@@ -8,8 +8,8 @@ package com.itheima.git;
  */
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello");
 
+        System.out.println("hello");
 
     }
     public void test(){
